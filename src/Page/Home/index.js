a201680fbd251826f5745e3b5364fd36f66f12a8
Motasim Foad <motasimfoad@gmail.com>
2020-05-01 05:45:22 +0600

    Added timer

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -15,9 +15,23 @@ function App() {
   const [currentScore, setCurrentScore] = useState(10);
   const [currentText, setCurrentText] = useState(randomText());
   const [userText, setUserText] = useState('');
+  const [started, setStarted] = useState(false);
+  const [time, setTime] = useState(0);
 
   const onChange = (e) => {
       setUserText(e.currentTarget.value);
+      timer();
+  };
+
+  const timer = () => {
+    if (!started) {
+        setStarted(true);
+        setInterval(() => {
+            setTime(time => time + 1);  
+            }, 1000);
+      }
+      console.log(time);
+      
   };
 
   return (
@@ -26,8 +40,9 @@ function App() {
            <Col xl={6}>
            <TextArea currentText={currentText} userText={userText}/>
            <div>
-          <input text="yoyo" type="text" placeholder={userText} onChange={onChange}/>
+          <input text="yoyo" type="text" placeholder="Input here" onChange={onChange}/>
           </div>
+          {time}
            </Col>
            <Col xl={6}>
            <Score currentScore={currentScore}/>
