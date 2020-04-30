970265e3f70c007009a978f96adaf2ab7c16a411
Motasim Foad <motasimfoad@gmail.com>
2020-05-01 04:02:09 +0600

    Added game color

--- src/Components/TextArea/index.js
+++ src/Components/TextArea/index.js
@@ -4,21 +4,29 @@ import '../TextArea/textarea.css';
 
 function TextArea(props) {
     const [currentText, setCurrentText] = useState(props.currentText);
+    const [testText, setTestText] = useState(currentText.split());
     const [userText, setUserText] = useState(props.userText);
     
     useEffect(() => {
         setUserText(props.userText);
     })
 
-    
-
-
-
   return (
   
        <Row>
            <Col xl={12}>
            <Jumbotron>
+               {
+                   testText.map((s,i) => {
+                       let color;
+                       if (i < userText.length){
+                        color = s === userText[i] ? 'red' : 'green';
+                        console.log(userText.length);
+                        
+                       }
+                       return <span key={i} style={{backgroundColor: color}}>{s}<br /></span>
+                   })
+               }
                 {currentText} <br />
                 {userText}
            </Jumbotron>
